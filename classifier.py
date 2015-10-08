9ecc3fc8e33801ac6319c9d970712754e6b09b3e
Yousra Bekhti <yousra.bekhti@gmail.com>
2015-10-08 12:01:03 +0200

    Mainak_model

--- classifier.py
+++ classifier.py
@@ -1,29 +1,76 @@
+# All the imports
 import os
 os.environ["THEANO_FLAGS"] = "device=gpu"
-from sklearn.base import BaseEstimator
-import os
-from lasagne import layers, nonlinearities
-from lasagne.updates import nesterov_momentum
+
+from sklearn.pipeline import make_pipeline
+from caffezoo.googlenet import GoogleNet
 from nolearn.lasagne import NeuralNet, BatchIterator
-import numpy as np
 from nolearn.lasagne.handlers import EarlyStopping
+from lasagne import layers, nonlinearities
+from sklearn.base import BaseEstimator
+from sklearn.ensemble import RandomForestClassifier
+from skimage.transform import rotate
+from sklearn.cross_validation import KFold
+
+class ClassifierFlipRotate(BaseEstimator):
+    def __init__(self):
+        self.net = build_model(hyper_parameters)
+ 
+    def preprocess(self, X):
+        X = (X / 255.)
+        X = X.astype(np.float32)
+        X = X.transpose((0, 3, 1, 2))
+        return X
+ 
+    def preprocess_y(self, y):
+        return y.astype(np.int32)
+ 
+    def fit(self, X, y):
+        X = self.preprocess(X)
+        self.net.fit(X, self.preprocess_y(y))
+        return self
+ 
+    def predict(self, X):
+        X = self.preprocess(X)
+        return self.net.predict(X)
+
+    def predict_proba(self, X):
+        X = self.preprocess(X)
+        return self.net.predict_proba(X)
 
 
-class FlipBatchIterator(BatchIterator):
+class FlipRotateBatchIterator(BatchIterator):
     def transform(self, Xb, yb):
-        Xb, yb = super(FlipBatchIterator, self).transform(Xb, yb)
+        Xb, yb = super(FlipRotateBatchIterator, self).transform(Xb, yb)
         # Flip half of the images in this batch at random:
         bs = Xb.shape[0]
-        indices = np.random.choice(bs, bs / 2, replace=False)
-        Xb[indices] = Xb[indices]
+        # indices = np.random.choice(bs, bs / 2, replace=False)
+        cv = KFold(bs, 3, shuffle=True)
+        for ii, (_, indices) in enumerate(cv):
+            if ii == 0:
+                Xb[indices] = Xb[indices, :, ::-1, :]
+            if ii == 1:
+                for index in indices:
+                    Xb[index] = rotate(Xb[index, ...], 90)
+            if ii == 2:
+                for index in indices:
+                    Xb[index] = rotate(Xb[index, ...], 180)
 
-        # Drop randomly half of the features in each batch:
-        bf = Xb.shape[2]
-        indices_features = np.random.choice(bf, bf / 2, replace=False)
-        Xb = Xb.transpose((2, 0, 1, 3))
-        Xb[indices_features] = Xb[indices_features]
-        Xb = Xb.transpose((1, 2, 0, 3))
         return Xb, yb
+
+hyper_parameters = dict(
+    conv1_num_filters=64, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
+    conv2_num_filters=128, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
+    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(4, 4),
+    hidden4_num_units=500, hidden5_num_units=500,
+    dropout5_p=0.5,
+    output_num_units=18, output_nonlinearity=nonlinearities.softmax,
+    update_learning_rate=0.01,
+    update_momentum=0.9,
+    max_epochs=20,
+    on_epoch_finished=[EarlyStopping(patience=20, criterion='valid_accuracy', criterion_smaller_is_better=False)],
+    batch_iterator_train=FlipRotateBatchIterator(batch_size=256 * 3)
+)
 
 
 def build_model(hyper_parameters):
@@ -47,44 +94,3 @@ def build_model(hyper_parameters):
         **hyper_parameters
         )
     return net
-
-hyper_parameters = dict(
-    conv1_num_filters=64, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
-    conv2_num_filters=128, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
-    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(4, 4),
-    hidden4_num_units=500, hidden5_num_units=500,
-    dropout5_p=0.5,
-    output_num_units=18, output_nonlinearity=nonlinearities.softmax,
-    update_learning_rate=0.01,
-    update_momentum=0.9,
-    max_epochs=200,
-    on_epoch_finished=[EarlyStopping(patience=20, criterion='valid_accuracy', criterion_smaller_is_better=False)],
-    batch_iterator_train=FlipBatchIterator(batch_size=256)
-)
-
-
-class Classifier(BaseEstimator):
-    def __init__(self):
-        self.net = build_model(hyper_parameters)
-
-    def preprocess(self, X):
-        X = (X / 255.)
-        X = X.astype(np.float32)
-        X = X.transpose((0, 3, 1, 2))
-        return X
-
-    def preprocess_y(self, y):
-        return y.astype(np.int32)
-
-    def fit(self, X, y):
-        X = self.preprocess(X)
-        self.net.fit(X, self.preprocess_y(y))
-        return self
-
-    def predict(self, X):
-        X = self.preprocess(X)
-        return self.net.predict(X)
-
-    def predict_proba(self, X):
-        X = self.preprocess(X)
-        return self.net.predict_proba(X)
