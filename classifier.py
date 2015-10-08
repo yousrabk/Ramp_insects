8563b2198eb8be784149d19a428f59a6c37e1683
Yousra Bekhti <yousra.bekhti@gmail.com>
2015-10-08 11:57:06 +0200

    model_2

--- classifier.py
+++ classifier.py
@@ -1,21 +1,71 @@
 import os
 os.environ["THEANO_FLAGS"] = "device=gpu"
-
-import numpy as np
-
-from sklearn.pipeline import make_pipeline
-from caffezoo.googlenet import GoogleNet
 from sklearn.base import BaseEstimator
-from sklearn.ensemble import RandomForestClassifier
+import os
+from lasagne import layers, nonlinearities
+from lasagne.updates import nesterov_momentum
+from nolearn.lasagne import NeuralNet, BatchIterator
+import numpy as np
+from nolearn.lasagne.handlers import EarlyStopping
+
+
+class FlipBatchIterator(BatchIterator):
+    def transform(self, Xb, yb):
+        Xb, yb = super(FlipBatchIterator, self).transform(Xb, yb)
+        # Flip half of the images in this batch at random:
+        bs = Xb.shape[0]
+        indices = np.random.choice(bs, bs / 2, replace=False)
+        Xb[indices] = Xb[indices]
+
+        # Drop randomly half of the features in each batch:
+        bf = Xb.shape[2]
+        indices_features = np.random.choice(bf, bf / 2, replace=False)
+        Xb = Xb.transpose((2, 0, 1, 3))
+        Xb[indices_features] = Xb[indices_features]
+        Xb = Xb.transpose((1, 2, 0, 3))
+        return Xb, yb
+
+
+def build_model(hyper_parameters):
+    net = NeuralNet(
+        layers=[
+            ('input', layers.InputLayer),
+            ('conv1', layers.Conv2DLayer),
+            ('pool1', layers.MaxPool2DLayer),
+            ('conv2', layers.Conv2DLayer),
+            ('pool2', layers.MaxPool2DLayer),
+            ('conv3', layers.Conv2DLayer),
+            ('pool3', layers.MaxPool2DLayer),
+            ('hidden4', layers.DenseLayer),
+            ('hidden5', layers.DenseLayer),
+            ('dropout5', layers.DropoutLayer),
+            ('output', layers.DenseLayer),
+            ],
+        input_shape=(None, 3, 64, 64),
+        use_label_encoder=True,
+        verbose=1,
+        **hyper_parameters
+        )
+    return net
+
+hyper_parameters = dict(
+    conv1_num_filters=64, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
+    conv2_num_filters=128, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
+    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(4, 4),
+    hidden4_num_units=500, hidden5_num_units=500,
+    dropout5_p=0.5,
+    output_num_units=18, output_nonlinearity=nonlinearities.softmax,
+    update_learning_rate=0.01,
+    update_momentum=0.9,
+    max_epochs=200,
+    on_epoch_finished=[EarlyStopping(patience=20, criterion='valid_accuracy', criterion_smaller_is_better=False)],
+    batch_iterator_train=FlipBatchIterator(batch_size=256)
+)
 
 
 class Classifier(BaseEstimator):
-
     def __init__(self):
-        self.clf = make_pipeline(
-            GoogleNet(layer_names=["inception_4e/output"]),
-            RandomForestClassifier(n_estimators=100, max_depth=25)
-        )
+        self.net = build_model(hyper_parameters)
 
     def preprocess(self, X):
         X = (X / 255.)
@@ -23,12 +73,18 @@ class Classifier(BaseEstimator):
         X = X.transpose((0, 3, 1, 2))
         return X
 
+    def preprocess_y(self, y):
+        return y.astype(np.int32)
+
     def fit(self, X, y):
-        self.clf.fit(X, y)
+        X = self.preprocess(X)
+        self.net.fit(X, self.preprocess_y(y))
         return self
 
     def predict(self, X):
-        return self.clf.predict(X)
+        X = self.preprocess(X)
+        return self.net.predict(X)
 
     def predict_proba(self, X):
-        return self.clf.predict_proba(X)
+        X = self.preprocess(X)
+        return self.net.predict_proba(X)
