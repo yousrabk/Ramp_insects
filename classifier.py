8c57579d101cb1415f61a3a266521ae59f0429e7
Yousra Bekhti <yousra.bekhti@gmail.com>
2015-10-08 11:10:59 +0200

    model_1

--- classifier.py
+++ classifier.py
@@ -1,46 +1,21 @@
 import os
 os.environ["THEANO_FLAGS"] = "device=gpu"
-from sklearn.base import BaseEstimator
-from lasagne import layers, nonlinearities
-from nolearn.lasagne import NeuralNet
+
 import numpy as np
 
-
-def build_model(hyper_parameters):
-    net = NeuralNet(
-        layers=[
-            ('input', layers.InputLayer),
-            ('conv1', layers.Conv2DLayer),
-            ('pool1', layers.MaxPool2DLayer),
-            ('conv2', layers.Conv2DLayer),
-            ('pool2', layers.MaxPool2DLayer),
-            ('conv3', layers.Conv2DLayer),
-            ('pool3', layers.MaxPool2DLayer),
-            ('hidden4', layers.DenseLayer),
-            ('hidden5', layers.DenseLayer),
-            ('output', layers.DenseLayer)],
-        input_shape=(None, 3, 64, 64),
-        use_label_encoder=True,
-        verbose=1,
-        **hyper_parameters)
-    return net
-
-hyper_parameters = dict(
-    conv1_num_filters=64, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
-    conv2_num_filters=128, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
-    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
-    hidden4_num_units=500, hidden5_num_units=500,
-    output_num_units=18, output_nonlinearity=nonlinearities.softmax,
-    update_learning_rate=0.01,
-    update_momentum=0.9,
-    max_epochs=2,
-)
+from sklearn.pipeline import make_pipeline
+from caffezoo.googlenet import GoogleNet
+from sklearn.base import BaseEstimator
+from sklearn.ensemble import RandomForestClassifier
 
 
 class Classifier(BaseEstimator):
 
     def __init__(self):
-        self.net = build_model(hyper_parameters)
+        self.clf = make_pipeline(
+            GoogleNet(layer_names=["inception_4e/output"]),
+            RandomForestClassifier(n_estimators=100, max_depth=25)
+        )
 
     def preprocess(self, X):
         X = (X / 255.)
@@ -49,14 +24,11 @@ class Classifier(BaseEstimator):
         return X
 
     def fit(self, X, y):
-        X = self.preprocess(X)
-        self.net.fit(X, y)
+        self.clf.fit(X, y)
         return self
 
     def predict(self, X):
-        X = self.preprocess(X)
-        return self.net.predict(X)
+        return self.clf.predict(X)
 
     def predict_proba(self, X):
-        X = self.preprocess(X)
-        return self.net.predict_proba(X)
+        return self.clf.predict_proba(X)
